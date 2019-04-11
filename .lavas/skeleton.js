595c56e78de6465c9b345eade7f1198ed1f8fdc0
dashuai <86yhs@163.com>
2019-04-11 14:26:51 +0800

    这是原封不动的lavas文件

---
+++ .lavas/skeleton.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+
+import Skeleton from 'D:\\agit1901\\lavas\\lavas-2-sample\\core\\Skeleton.vue';
+
+
+export default new Vue({
+    components: {
+        
+        Skeleton,
+        
+    },
+    template: `
+        <div>
+            
+            <skeleton id="skeleton" style="display:none"/>
+            
+        </div>
+    `
+});
