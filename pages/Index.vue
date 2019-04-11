8a6edc7f16054a013796016707b4b660b0e42832
dashuai <86yhs@163.com>
2019-04-11 14:55:36 +0800

    修改了meta信息，测试从vscode上传的功能

--- pages/Index.vue
+++ pages/Index.vue
@@ -30,8 +30,8 @@ function setState(store) {
 export default {
     name: 'index',
     metaInfo: {
-        title: 'Home',
-        titleTemplate: '%s - Lavas',
+        title: 'Home,yhs',
+        titleTemplate: '%s -dongdong- Lavas',
         meta: [
             {name: 'keywords', content: 'lavas PWA'},
             {name: 'description', content: '基于 Vue 的 PWA 解决方案，帮助开发者快速搭建 PWA 应用，解决接入 PWA 的各种问题'}
